2fc3cc212c5b0197c0f5de42971512ece0497586
yewon <namyes16@gmail.com>
2024-10-17 20:14:51 +0900

    색 변경

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,10 +29,10 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.lblProgramName = new System.Windows.Forms.Label();
             this.pnlMenu = new System.Windows.Forms.Panel();
-            this.btnTableOfContents = new System.Windows.Forms.Button();
-            this.btnMorningWorshipPPT = new System.Windows.Forms.Button();
-            this.btnIndividualHymnsPPT = new System.Windows.Forms.Button();
             this.btnChoirLyricsInput = new System.Windows.Forms.Button();
+            this.btnIndividualHymnsPPT = new System.Windows.Forms.Button();
+            this.btnMorningWorshipPPT = new System.Windows.Forms.Button();
+            this.btnTableOfContents = new System.Windows.Forms.Button();
             this.pnlDesktop = new System.Windows.Forms.Panel();
             this.morningWorshipPPT1 = new Worship.MorningWorshipPPT();
             this.openingHymnsPPT1 = new Worship.OpeningHymnsPPT();
@@ -44,7 +44,7 @@
             // 
             // pnlTitleBar
             // 
-            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(46)))));
+            this.pnlTitleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(232)))));
             this.pnlTitleBar.Controls.Add(this.btnMaximize);
             this.pnlTitleBar.Controls.Add(this.btnMinimize);
             this.pnlTitleBar.Controls.Add(this.btnClose);
@@ -52,7 +52,7 @@
             this.pnlTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlTitleBar.Location = new System.Drawing.Point(0, 0);
             this.pnlTitleBar.Name = "pnlTitleBar";
-            this.pnlTitleBar.Size = new System.Drawing.Size(1618, 29);
+            this.pnlTitleBar.Size = new System.Drawing.Size(1395, 29);
             this.pnlTitleBar.TabIndex = 2;
             this.pnlTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTitleBar_MouseDown);
             // 
@@ -62,8 +62,8 @@
             this.btnMaximize.FlatAppearance.BorderSize = 0;
             this.btnMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMaximize.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnMaximize.ForeColor = System.Drawing.Color.White;
-            this.btnMaximize.Location = new System.Drawing.Point(1557, 3);
+            this.btnMaximize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnMaximize.Location = new System.Drawing.Point(1334, 3);
             this.btnMaximize.Name = "btnMaximize";
             this.btnMaximize.Size = new System.Drawing.Size(25, 25);
             this.btnMaximize.TabIndex = 2;
@@ -77,8 +77,8 @@
             this.btnMinimize.FlatAppearance.BorderSize = 0;
             this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMinimize.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnMinimize.ForeColor = System.Drawing.Color.White;
-            this.btnMinimize.Location = new System.Drawing.Point(1526, 3);
+            this.btnMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnMinimize.Location = new System.Drawing.Point(1303, 3);
             this.btnMinimize.Name = "btnMinimize";
             this.btnMinimize.Size = new System.Drawing.Size(25, 25);
             this.btnMinimize.TabIndex = 3;
@@ -92,8 +92,8 @@
             this.btnClose.FlatAppearance.BorderSize = 0;
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(1588, 4);
+            this.btnClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnClose.Location = new System.Drawing.Point(1365, 4);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(25, 25);
             this.btnClose.TabIndex = 1;
@@ -106,8 +106,8 @@
             this.lblProgramName.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lblProgramName.AutoSize = true;
             this.lblProgramName.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.lblProgramName.ForeColor = System.Drawing.Color.White;
-            this.lblProgramName.Location = new System.Drawing.Point(743, 6);
+            this.lblProgramName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.lblProgramName.Location = new System.Drawing.Point(631, 6);
             this.lblProgramName.Name = "lblProgramName";
             this.lblProgramName.Size = new System.Drawing.Size(133, 17);
             this.lblProgramName.TabIndex = 0;
@@ -115,7 +115,7 @@
             // 
             // pnlMenu
             // 
-            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(65)))));
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(247)))));
             this.pnlMenu.Controls.Add(this.btnChoirLyricsInput);
             this.pnlMenu.Controls.Add(this.btnIndividualHymnsPPT);
             this.pnlMenu.Controls.Add(this.btnMorningWorshipPPT);
@@ -123,59 +123,8 @@
             this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlMenu.Location = new System.Drawing.Point(0, 0);
             this.pnlMenu.Name = "pnlMenu";
-            this.pnlMenu.Size = new System.Drawing.Size(1618, 28);
+            this.pnlMenu.Size = new System.Drawing.Size(1395, 28);
             this.pnlMenu.TabIndex = 0;
-            // 
-            // btnTableOfContents
-            // 
-            this.btnTableOfContents.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnTableOfContents.FlatAppearance.BorderSize = 0;
-            this.btnTableOfContents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnTableOfContents.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnTableOfContents.ForeColor = System.Drawing.Color.White;
-            this.btnTableOfContents.Location = new System.Drawing.Point(0, 0);
-            this.btnTableOfContents.Name = "btnTableOfContents";
-            this.btnTableOfContents.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnTableOfContents.Size = new System.Drawing.Size(60, 28);
-            this.btnTableOfContents.TabIndex = 4;
-            this.btnTableOfContents.Text = "목차";
-            this.btnTableOfContents.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnTableOfContents.UseVisualStyleBackColor = true;
-            this.btnTableOfContents.Click += new System.EventHandler(this.btnTableOfContents_Click);
-            // 
-            // btnMorningWorshipPPT
-            // 
-            this.btnMorningWorshipPPT.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnMorningWorshipPPT.FlatAppearance.BorderSize = 0;
-            this.btnMorningWorshipPPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMorningWorshipPPT.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnMorningWorshipPPT.ForeColor = System.Drawing.Color.White;
-            this.btnMorningWorshipPPT.Location = new System.Drawing.Point(60, 0);
-            this.btnMorningWorshipPPT.Name = "btnMorningWorshipPPT";
-            this.btnMorningWorshipPPT.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnMorningWorshipPPT.Size = new System.Drawing.Size(108, 28);
-            this.btnMorningWorshipPPT.TabIndex = 1;
-            this.btnMorningWorshipPPT.Text = "오전예배 PPT";
-            this.btnMorningWorshipPPT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnMorningWorshipPPT.UseVisualStyleBackColor = true;
-            this.btnMorningWorshipPPT.Click += new System.EventHandler(this.btnMorningWorshipPPT_Click);
-            // 
-            // btnIndividualHymnsPPT
-            // 
-            this.btnIndividualHymnsPPT.Dock = System.Windows.Forms.DockStyle.Left;
-            this.btnIndividualHymnsPPT.FlatAppearance.BorderSize = 0;
-            this.btnIndividualHymnsPPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnIndividualHymnsPPT.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnIndividualHymnsPPT.ForeColor = System.Drawing.Color.White;
-            this.btnIndividualHymnsPPT.Location = new System.Drawing.Point(168, 0);
-            this.btnIndividualHymnsPPT.Name = "btnIndividualHymnsPPT";
-            this.btnIndividualHymnsPPT.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
-            this.btnIndividualHymnsPPT.Size = new System.Drawing.Size(123, 28);
-            this.btnIndividualHymnsPPT.TabIndex = 3;
-            this.btnIndividualHymnsPPT.Text = "시작 찬송가 PPT";
-            this.btnIndividualHymnsPPT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnIndividualHymnsPPT.UseVisualStyleBackColor = true;
-            this.btnIndividualHymnsPPT.Click += new System.EventHandler(this.btnIndividualHymnsPPT_Click);
             // 
             // btnChoirLyricsInput
             // 
@@ -183,7 +132,7 @@
             this.btnChoirLyricsInput.FlatAppearance.BorderSize = 0;
             this.btnChoirLyricsInput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnChoirLyricsInput.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnChoirLyricsInput.ForeColor = System.Drawing.Color.White;
+            this.btnChoirLyricsInput.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
             this.btnChoirLyricsInput.Location = new System.Drawing.Point(291, 0);
             this.btnChoirLyricsInput.Name = "btnChoirLyricsInput";
             this.btnChoirLyricsInput.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
@@ -194,55 +143,107 @@
             this.btnChoirLyricsInput.UseVisualStyleBackColor = true;
             this.btnChoirLyricsInput.Click += new System.EventHandler(this.btnChoirLyricsInput_Click);
             // 
+            // btnIndividualHymnsPPT
+            // 
+            this.btnIndividualHymnsPPT.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnIndividualHymnsPPT.FlatAppearance.BorderSize = 0;
+            this.btnIndividualHymnsPPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIndividualHymnsPPT.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnIndividualHymnsPPT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnIndividualHymnsPPT.Location = new System.Drawing.Point(168, 0);
+            this.btnIndividualHymnsPPT.Name = "btnIndividualHymnsPPT";
+            this.btnIndividualHymnsPPT.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnIndividualHymnsPPT.Size = new System.Drawing.Size(123, 28);
+            this.btnIndividualHymnsPPT.TabIndex = 3;
+            this.btnIndividualHymnsPPT.Text = "시작 찬송가 PPT";
+            this.btnIndividualHymnsPPT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnIndividualHymnsPPT.UseVisualStyleBackColor = true;
+            this.btnIndividualHymnsPPT.Click += new System.EventHandler(this.btnIndividualHymnsPPT_Click);
+            // 
+            // btnMorningWorshipPPT
+            // 
+            this.btnMorningWorshipPPT.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnMorningWorshipPPT.FlatAppearance.BorderSize = 0;
+            this.btnMorningWorshipPPT.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMorningWorshipPPT.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnMorningWorshipPPT.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnMorningWorshipPPT.Location = new System.Drawing.Point(60, 0);
+            this.btnMorningWorshipPPT.Name = "btnMorningWorshipPPT";
+            this.btnMorningWorshipPPT.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnMorningWorshipPPT.Size = new System.Drawing.Size(108, 28);
+            this.btnMorningWorshipPPT.TabIndex = 1;
+            this.btnMorningWorshipPPT.Text = "오전예배 PPT";
+            this.btnMorningWorshipPPT.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMorningWorshipPPT.UseVisualStyleBackColor = true;
+            this.btnMorningWorshipPPT.Click += new System.EventHandler(this.btnMorningWorshipPPT_Click);
+            // 
+            // btnTableOfContents
+            // 
+            this.btnTableOfContents.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnTableOfContents.FlatAppearance.BorderSize = 0;
+            this.btnTableOfContents.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTableOfContents.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnTableOfContents.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.btnTableOfContents.Location = new System.Drawing.Point(0, 0);
+            this.btnTableOfContents.Name = "btnTableOfContents";
+            this.btnTableOfContents.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnTableOfContents.Size = new System.Drawing.Size(60, 28);
+            this.btnTableOfContents.TabIndex = 4;
+            this.btnTableOfContents.Text = "목차";
+            this.btnTableOfContents.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTableOfContents.UseVisualStyleBackColor = true;
+            this.btnTableOfContents.Click += new System.EventHandler(this.btnTableOfContents_Click);
+            // 
             // pnlDesktop
             // 
-            this.pnlDesktop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.pnlDesktop.BackColor = System.Drawing.Color.White;
             this.pnlDesktop.Controls.Add(this.choirLyricsInput1);
             this.pnlDesktop.Controls.Add(this.openingHymnsPPT1);
             this.pnlDesktop.Controls.Add(this.morningWorshipPPT1);
             this.pnlDesktop.Controls.Add(this.pnlMenu);
-            this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlDesktop.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnlDesktop.Location = new System.Drawing.Point(0, 29);
             this.pnlDesktop.Name = "pnlDesktop";
-            this.pnlDesktop.Size = new System.Drawing.Size(1618, 716);
+            this.pnlDesktop.Size = new System.Drawing.Size(1395, 716);
             this.pnlDesktop.TabIndex = 1;
             // 
             // morningWorshipPPT1
             // 
+            this.morningWorshipPPT1.BackColor = System.Drawing.Color.White;
             this.morningWorshipPPT1.Dock = System.Windows.Forms.DockStyle.Left;
             this.morningWorshipPPT1.Location = new System.Drawing.Point(0, 28);
             this.morningWorshipPPT1.Name = "morningWorshipPPT1";
-            this.morningWorshipPPT1.Size = new System.Drawing.Size(492, 688);
-            this.morningWorshipPPT1.TabIndex = 5;
+            this.morningWorshipPPT1.Size = new System.Drawing.Size(465, 688);
+            this.morningWorshipPPT1.TabIndex = 4;
             // 
             // openingHymnsPPT1
             // 
+            this.openingHymnsPPT1.BackColor = System.Drawing.Color.White;
             this.openingHymnsPPT1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.openingHymnsPPT1.Location = new System.Drawing.Point(492, 28);
+            this.openingHymnsPPT1.Location = new System.Drawing.Point(465, 28);
             this.openingHymnsPPT1.Name = "openingHymnsPPT1";
-            this.openingHymnsPPT1.Size = new System.Drawing.Size(492, 688);
-            this.openingHymnsPPT1.TabIndex = 6;
+            this.openingHymnsPPT1.Size = new System.Drawing.Size(465, 688);
+            this.openingHymnsPPT1.TabIndex = 5;
             // 
             // choirLyricsInput1
             // 
-            this.choirLyricsInput1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(27)))), ((int)(((byte)(28)))), ((int)(((byte)(31)))));
+            this.choirLyricsInput1.BackColor = System.Drawing.Color.White;
             this.choirLyricsInput1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.choirLyricsInput1.Location = new System.Drawing.Point(984, 28);
+            this.choirLyricsInput1.Location = new System.Drawing.Point(930, 28);
             this.choirLyricsInput1.Name = "choirLyricsInput1";
-            this.choirLyricsInput1.Size = new System.Drawing.Size(492, 688);
-            this.choirLyricsInput1.TabIndex = 7;
+            this.choirLyricsInput1.Size = new System.Drawing.Size(465, 688);
+            this.choirLyricsInput1.TabIndex = 6;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1618, 745);
+            this.ClientSize = new System.Drawing.Size(1395, 757);
             this.Controls.Add(this.pnlDesktop);
             this.Controls.Add(this.pnlTitleBar);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.MinimumSize = new System.Drawing.Size(508, 576);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Worship";
